e38720b4a76c7239d65b14f7baac9e2d64e4ed42
monicazng <roumonicazeng@gmail.com>
2015-12-26 08:43:19 +0100

    Update captions.js

--- captions.js
+++ captions.js
@@ -1,22 +1,37 @@
 var Captions = [
   {
-    title: 'Gato blanco sobre la nieve',
-    text: "No text also means no context. You're free to enjoy the images in their purest form, without names, labels, definitions, or purpose. It makes the pictures we see across the web more mysterious and open to interpretation of our own imaginations.",
+    title: 'A Sunday afternoon on the island of La Grande Jatte by Seurat-Sand Reply',
+    text: "67x50-Sand.They used to say that a good artist copies and a great artist steals. So that was my approach when creating this reply of Seurat's painting:stealing his idea with my own interpretation of pointillism. With sand colored by myself,here's another way to visually mix this technique's patterns.",
     buy: 'https://gum.co/ZVmf'
   },
   {
-    title: 'Gato blanco sobre el lienzo',
-    text: "Complete with a plethora of dark, juicy, sexually charged imagery for you to chew on, this performance piece reveals the mind of one particular cowboy slipping into uncharted subconscious territory where he shifts into dangerous deep rooted curiosities. A place where archetypes collide. A place where his cowboyness... turns.",
+    title: 'Baby Splash',
+    text: "18x15x12-Clay+Acrylics. Is there any being more powerful than a baby's will? This little sculpture was a true challenge mainly because no one trusted that it will stand. Clay,as fragile as children would look like but as resistant as their persistence to make possible what's impossible. Baby Splash was built piece by piece exaggerating the proportions of power focus and with special care of that frozen spash that freezes the little kid we bring inside us.",
     buy: 'https://gum.co/ZVmf'
   },
   {
-    title: 'Gato negro sobre el taper',
-    text: "The Immortal investigates human dependence on electronics, the desire to make machines replicate organisms and our perception of anatomy as reflected by biomedical engineering",
+    title: 'Brain wash',
+    text: "42x29.5-Pastel. I was randomly drawing something that even myself ignored when I thought about our society (you'll see several social criticism inspired artworks) and how no matter how we are, peer pressure will always try to change us till the point that personality becames socianality. No expression, no self-control and neither critical thoguht. Can you wonder if this person is a man or a woman? I definitly can't but it doesn't matter. The hands surrounding him/her will decide his/her destiny at the end.",
     buy: 'https://gum.co/ZVmf'
   },
   {
-    title: 'Gato gris sobre la nube',
-    text: "My work explores a love affair with the sustained gaze afforded the painter in a digital age of limited attention span. That and the attempt to somehow capture ‘life’ on a surface of paint and canvas.",
+    title: 'Dreams or nightmares?',
+    text: "38x49-Linoleum+Acrylics. How many times a day you complain? How many times a day you are thankful for what you have? Can you balance all variables in your life and adjudicate: I'm (not) happy? It's hard and at least in my case, I used to say that happiness is momentary because without comparing with bad times, we are not able to enjoy good times. A mix of colors as a mix of feelings and a technique with a heavy and unordered texture, as our thoguhts, likewise our dreams. Or they are nightmares?",
+    buy: 'https://gum.co/ZVmf'
+  },
+  {
+    title: 'Melting city',
+    text: "80x60-Acrylics. Human being is the most destroyable animal in the world. Here I wanted to represent the consequences of a society that lost its values and common sense: human being itself is a stair under money, profit and nature. We are developing at a deezing speed which makes nature impossible to regenerate itself. That leads us to an unsustainable development, in other words, we are destroying ourselves: with no nature, there's no life. The city (complete lack of green) represents this unsustainable development and the fire we used to burn the natural environment is the same fire melting our existence.",
+    buy: 'https://gum.co/ZVmf'
+  },
+  {
+    title: 'Red sky forest',
+    text: "70x50-Acrylics. I confese I love red because is a very powerful color; with it I feel both anger and joy, melancholy and cheerful memories, the power to make things happen and the frustration of what's hard to make possible. A simple but complex landscape inspired by calm and serenity, creating an invading environment of peacefulness. One of my loves.",
+    buy: 'https://gum.co/ZVmf'
+  },
+  {
+    title: 'Red sky eagle',
+    text: "50x40-Acrylics. If you ever ask me which animal I would be identified with, my answer will definitely be the eagle. And there's no better combination than with my loved red sky! Eagle's emperialism, independence, firm transmits me respect and admiration. I will never get tired to paint them.",
     buy: 'https://gum.co/ZVmf'
   }
 ]
